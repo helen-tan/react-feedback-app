a70e9258ad12acf8325baa6b920dae29f94d8660
helen tan <helentanmz0909@gmail.com>
2022-06-27 14:44:46 +0800

    Dynamic values & Lists in JSX

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// Compoennts can be either classes or functions
+// Components can be either classes or functions
 // classes - old school way of creating components
 // functions - newer way, functional components. Uses Hooks & states
 
@@ -6,11 +6,30 @@ function App() {
   // return JSX, a syntax that allows us to have syntactic sugar for putting HTML in JS
   // We can only return 1 main parent element
   // But if we don't want a wrapping div, so we can return an empty parent fragment <>
+
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'Comment one'},
+    { id: 2, text: 'Comment two'},
+    { id: 3, text: 'Comment three'}
+  ];
+
   return (
     // In JSX, class is a reserved keyword. We use className
     // In forms, for attribute in JSX is htmlFor
     <div className = 'container'>
-      <h1>My App</h1>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => {
+            <li key={index}>{comment.text}</li>
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
