99c81d9877be7aa703b095e901b4a67e939fac81
helen tan <helentanmz0909@gmail.com>
2022-06-28 15:00:07 +0800

    Added button component to feedback form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import Card from "./shared/Card"
+import Button from "./shared/Button"
 
 function FeedbackForm() {
   // When we have a form, we typically have a component-level state for each input
@@ -23,7 +24,7 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
             />
-          <button type="submit">Send</button>
+          <Button type="submit">Send</Button>
         </div>
       </form>
     </Card>
