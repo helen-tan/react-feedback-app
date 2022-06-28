15259d95b6b012719fb6dc5e820aba37cbce82a7
helen tan <helentanmz0909@gmail.com>
2022-06-28 15:16:43 +0800

    real-time form input validation: btn disabled + show msg when typing and when char <10

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -8,10 +8,22 @@ function FeedbackForm() {
   const [btnDisabled, setBtnDisabled] = useState(true) // true by default - btn should be disabled when page first loads
   const [message, setMessage] = useState('')
 
-
   // connect the state to the input value - use event listener onChange in the input
-  // create onChange event handler - handleTextChange: changes the 'text' state
+  // create onChange event handler - handleTextChange
   const handleTextChange = (e) => {
+    // validation to run when we type something
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null) // message to only show when we are typing & less than 10 chars
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters')
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+
+    // update the 'text' state
     setText(e.target.value);
   }
 
@@ -27,8 +39,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
             />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {/*If there is a message, display it */}
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
