573210eeadd87ede3cadbb156f6a402eab22b337
helen tan <helentanmz0909@gmail.com>
2022-06-28 15:49:45 +0800

    Update the rating state with the value received from RatingSelect component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -34,7 +34,7 @@ function FeedbackForm() {
       <form>
         <h2>How would you rate your service with us?</h2>
 
-        <RatingSelect select={(rating) => console.log(rating)} />
+        <RatingSelect select={(rating) => setRating(rating)} />
 
         <div className="input-group">
           <input
